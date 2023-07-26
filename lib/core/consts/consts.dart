0e7a8961958a15422b18167df9f854a7f889ea9f
sgsm74 <saeed.razii93@gmail.com>
2023-07-26 11:29:20 +0330

    chore: add authentication server endpoints

---
+++ lib/core/consts/consts.dart
@@ -0,0 +1,6 @@
+class ServerPaths {
+  ServerPaths._();
+  static String sendOTP = 'v1/auth/otp';
+  static String verifyOTP = 'v1/auth/otp/verify';
+  static String refreshJWTToken = 'v1/auth/jwt/refresh';
+}
